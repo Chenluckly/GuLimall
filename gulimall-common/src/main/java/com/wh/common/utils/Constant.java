3143fad03177b0f8c8989c8698fc8f6fc8406d87
wanghu <2583661719@qq.com>
2022-03-27 19:28:28 +0800

    第五次

--- gulimall-common/src/main/java/com/wh/common/utils/Constant.java
+++ gulimall-common/src/main/java/com/wh/common/utils/Constant.java
@@ -8,10 +8,9 @@
 
 package com.wh.common.utils;
 
-
-import io.renren.common.validator.group.AliyunGroup;
-import io.renren.common.validator.group.QcloudGroup;
-import io.renren.common.validator.group.QiniuGroup;
+import com.wh.common.utils.validator.group.AliyunGroup;
+import com.wh.common.utils.validator.group.QcloudGroup;
+import com.wh.common.utils.validator.group.QiniuGroup;
 
 import java.util.Optional;
 import java.util.stream.Stream;
